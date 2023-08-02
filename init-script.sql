c5c7a831b75e1cb749006aca4506d1fe0ba6db83
Andres Caicedo <andres.felipe.caicedo.ultengo@outlook.com>
2023-08-02 20:47:37 +0200

    Update init-script.sql

--- init-script.sql
+++ init-script.sql
@@ -1,8 +1,47 @@
+
+-- Path: init-script.sql
+
 /* 
- Ini
+ Init script for the database
 */
 
 EXEC sp_configure 'remote access', 0;
 GO  
-RECONFIGURE;  
+RECONFIGURE;
+GO
+
+EXEC sp_configure 'remote query timeout', 600;
+GO
+RECONFIGURE;
+GO
+
+EXEC sp_configure 'show advanced options', 1;
+GO
+RECONFIGURE;
+GO
+
+EXEC sp_configure 'xp_cmdshell', 1;
+GO
+RECONFIGURE;
+GO
+
+EXEC sp_configure 'show advanced options', 0;
+GO
+RECONFIGURE;
+GO
+
+EXEC sp_configure 'clr enabled', 1;
+GO
+RECONFIGURE;
+GO
+
+EXEC sp_configure 'clr strict security', 0;
+GO
+RECONFIGURE;
+GO
+
+CREATE DATABASE Torneo;
+GO
+
+USE Torneo;
 GO

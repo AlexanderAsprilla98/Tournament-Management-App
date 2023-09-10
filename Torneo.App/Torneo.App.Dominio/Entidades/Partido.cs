5711cb322c41a4f32efbfe0b30a8ab1cf301afa1
Alexander Asprilla <asprilla9819@gmail.com>
2023-09-10 04:33:07 -0500

    Data Annotation Partido - fix bug null on create

--- Torneo.App/Torneo.App.Dominio/Entidades/Partido.cs
+++ Torneo.App/Torneo.App.Dominio/Entidades/Partido.cs
@@ -1,12 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
 namespace Torneo.App.Dominio
+
+
 {
     public class Partido
     {
         public int Id { get; set; }
+
+        //[RegularExpression(@"^[\da-zA-Z]+$", ErrorMessage = "Valor Incorrecto. Ingrese solo letras")]
+        [Display(Name = "Fecha y hora del partido")]
+        [Required(ErrorMessage = "La fecha del partido es obligatoria.")]       
+        //[DisplayFormat(ConvertEmptyStringToNull=false)]
         public DateTime FechaHora { get; set; }
-        public Equipo Local { get; set; } 
+        
+        public Equipo? Local { get; set; } 
+
+        //[RegularExpression(@"^[\da-zA-Z]+$", ErrorMessage = "Valor Incorrecto. Ingrese solo letras")]
+        [Display(Name = "Marcador Local")]
+        [Required(ErrorMessage = "El Marcador Local es obligatorio.")]       
+        //[DisplayFormat(ConvertEmptyStringToNull=false)]
+        [Range(0, 100, ErrorMessage = "Marcador Local debe estar en un rango entre 0 y 100")]
         public int MarcadorLocal { get; set; }
-        public Equipo Visitante { get; set; } = new Equipo();
+        public Equipo? Visitante { get; set; } 
+
+        //[RegularExpression(@"^[\da-zA-Z]+$", ErrorMessage = "Valor Incorrecto. Ingrese solo letras")]
+        [Display(Name = "Marcador Visitante")]
+        [Required(ErrorMessage = "El Marcador Visitante es obligatoria.")]       
+        //[DisplayFormat(ConvertEmptyStringToNull=false)]
+        [Range(0, 100, ErrorMessage = "Marcador Visitante debe estar en un rango entre 0 y 100")]
         public int MarcadorVisitante { get; set; }
     }
 }

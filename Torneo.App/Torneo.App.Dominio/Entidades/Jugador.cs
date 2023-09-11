178144bf00f033126e9060cde0b387807a27e941
Alexander Asprilla <asprilla9819@gmail.com>
2023-09-11 03:35:52 -0500

    Data Annotation Jugador - fix bug null on create

--- Torneo.App/Torneo.App.Dominio/Entidades/Jugador.cs
+++ Torneo.App/Torneo.App.Dominio/Entidades/Jugador.cs
@@ -1,11 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Torneo.App.Dominio
 {
     public class Jugador
     {
         public int Id { get; set; }
-        public string Nombre { get; set; } = new string("");
+
+        [RegularExpression(@"^(?![0-9]+$)[a-zA-ZÀ-ÿ\s]+$", ErrorMessage = "Valor Incorrecto. Solo se permiten letras")]
+        [Display(Name = "Nombre del Jugador")]
+        [Required(AllowEmptyStrings=false, ErrorMessage = "El nombre del Jugador es obligatorio.")]             
+        [MaxLength(50, ErrorMessage = "El nombre del jugador no puede contener más de 50 caracteres")]
+        [MinLength(3, ErrorMessage = "El nombre del jugador no puede contener menos de 3 caracteres")] 
+        public string Nombre { get; set; }
+
+        [Display(Name = "Número del Jugador")]
+        [Required(AllowEmptyStrings=false, ErrorMessage = "El Número del Jugador es obligatorio.")]       
+        [DisplayFormat(ConvertEmptyStringToNull=false)]
+        [Range(1, 99, ErrorMessage = "El Número del Jugador debe estar en un rango entre 1 y 99")]
         public int Numero { get; set; }
-        public Equipo Equipo { get; set; } = new Equipo();
-        public Posicion Posicion { get; set; } = new Posicion();
+        public Equipo? Equipo { get; set; } 
+        public Posicion? Posicion { get; set; } 
     }
 }

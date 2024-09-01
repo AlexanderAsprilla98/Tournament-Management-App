8965e81ee8134f6c532d88c1ef4ac235532313a1
Alexander Asprilla <asprilla9819@gmail.com>
2024-08-31 19:59:20 -0500

    Cambio variable DB Azure

--- Torneo.App/Torneo.App.Persistencia/AppRepositorios/DataContext.cs
+++ Torneo.App/Torneo.App.Persistencia/AppRepositorios/DataContext.cs
@@ -25,10 +25,10 @@ namespace Torneo.App.Persistencia
                 optionsBuilder.UseSqlServer("Server=tcp:torneo-futbol.database.windows.net,1433;Initial Catalog=Torneo;Persist Security Info=False;User ID=admin1;Password='Torneo;App.';MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
                 // Read the SA_PASSWORD environment variable
-                string saPassword = Environment.GetEnvironmentVariable("MSSQL_SA_PASSWORD");
+                //string saPassword = Environment.GetEnvironmentVariable("MSSQL_SA_PASSWORD");
 
                 // Use the SA_PASSWORD in the connection string
-                optionsBuilder.UseSqlServer($"Server=tcp:sql-server,1433;Database=Torneo;User ID=sa;Password={saPassword};TrustServerCertificate=True;Connection Timeout=5;Initial Catalog=Torneo;Encrypt=False");
+                //optionsBuilder.UseSqlServer($"Server=tcp:sql-server,1433;Database=Torneo;User ID=sa;Password={saPassword};TrustServerCertificate=True;Connection Timeout=5;Initial Catalog=Torneo;Encrypt=False");
             }
         }
 

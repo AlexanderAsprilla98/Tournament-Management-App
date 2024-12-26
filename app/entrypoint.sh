50d2483ba3383eb0e3e95aa7f7a39734c09baeea
Alexander Asprilla <asprilla9819@gmail.com>
2024-12-26 01:27:53 -0500

    fix(db): upgrade Entity Framework Core packages to version 7.0.8 and adjust migration commands

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -8,19 +8,23 @@
 #    echo "Database 'Torneo.db' already exists."
 #fi
 
-
 cd /app/Torneo.App/Torneo.App.Persistencia
 echo "Remove migrations for Persistencia..."
+# Ignore errors if there are no migrations to remove
 dotnet ef migrations remove --context Torneo.App.Persistencia.DataContext || true
+# Ignore errors to ensure the script continues even if the migration already exists
+# Ignore errors if the migration already exists
 echo "Applying migrations for Persistencia..."
 dotnet ef migrations add InitialCreate --context Torneo.App.Persistencia.DataContext || true
 dotnet ef database update
 
 cd /app/Torneo.App/Torneo.App.Frontend
 echo "Remove migrations for Frontend..."
-dotnet ef migrations remove --context Torneo.App.Frontend.DataContext || true
+# Ignore errors if there are no migrations to remove
+dotnet ef migrations remove --context Torneo.App.Frontend.Areas.Identity.Data.IdentityDataContext || true
+# Ignore errors if the migration already exists
 echo "Applying migrations for Frontend..."
-dotnet ef migrations add CreateIdentitySchema --context Torneo.App.Frontend.DataContext || true
+dotnet ef migrations add CreateIdentitySchema --context Torneo.App.Frontend.Areas.Identity.Data.IdentityDataContext || true
 dotnet ef database update
 
 cd /app
